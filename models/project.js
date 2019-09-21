abe603c11d065b4b331841ac17f55b27916c95ee
Rofi Syahrul <srofi44@gmail.com>
2019-09-22 01:20:56 +0700

    querying and routing projects, and little code modification for pagination and stylesheets

--- models/project.js
+++ models/project.js
@@ -6,15 +6,78 @@ String.prototype.convert = function() {
 };
 
 module.exports = class Project {
-  constructor(userid, admin) {
+  constructor(pool, userid, admin, limit = 2) {
+    this.pool = pool;
     this.userid = userid;
     this.admin = admin;
+    this.limit = limit;
+    this.sqlPart =
+      "FROM users INNER JOIN members ON members.userid = users.userid INNER JOIN projects proj ON proj.projectid = members.projectid";
   }
 
-  find(columns = ["id", "name", "member"], constraints = {}) {
+  getConditional(constraints = {}) {
     // if the user is not admin, the user can see his/her projects only
     if (!this.admin) constraints.userid = this.userid;
 
+    let conditionals = [];
+    let userQuery, subsubquery, subquery;
+
+    if (constraints.userid) {
+      if (constraints.userid.toString().trim().length > 0) {
+        subsubquery = `SELECT projectid FROM members WHERE userid = ${constraints.userid}`;
+        subquery = `SELECT userid FROM members WHER projectid IN ( ${subsubquery} )`;
+        conditionals.push(
+          `users.userid IN ( ${subquery} ) AND proj.projectid IN ( ${subsubquery} )`
+        );
+      }
+    }
+
+    if (constraints.memberName) {
+      if (constraints.memberName.trim().length > 0) {
+        userQuery = `SELECT userid FROM users WHERE LOWER(concat(firstname, ' ', lastname)) = LOWER(${constraints.memberName.convert()})`;
+        subsubquery = `SELECT projectid FROM members WHERE userid IN ( ${userQuery} )`;
+        subquery = `SELECT userid FROM members WHERE projectid IN ( ${subsubquery} )`;
+        conditionals.push(
+          `users.userid IN ( ${subquery} ) AND proj.projectid IN ( ${subsubquery} )`
+        );
+      }
+    }
+
+    if (constraints.projectId) {
+      if (constraints.projectId.toString().trim().length > 0)
+        conditionals.push(`proj.projectid = ${constraints.projectId}`);
+    }
+
+    if (constraints.projectName) {
+      if (constraints.projectName.trim().length > 0) {
+        conditionals.push(
+          `LOWER(proj.projectname) LIKE %${constraints.projectName
+            .toLowerCase()
+            .convert()}%`
+        );
+      }
+    }
+
+    if (conditionals.length > 0)
+      this.conditional = ` WHERE ${conditionals.join(" AND ")}`;
+    else this.conditional = "";
+
+    return this;
+  }
+
+  countPages() {
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT COUNT(DISTINCT proj.projectid) nums ${this.sqlPart} ${this.conditional}`;
+      this.pool.query(sql, (err, res) => {
+        if (err) reject(err);
+        else {
+          resolve(Math.ceil(res.rows ? res.rows[0].nums / this.limit : 0));
+        }
+      });
+    });
+  }
+
+  find(columns = ["id", "name", "member"], offset = 0) {
     const pg = {
       id: "proj.projectid",
       name: "proj.projectname",
@@ -22,39 +85,19 @@ module.exports = class Project {
     };
     const colPg = columns.map(col => pg[col]);
 
-    let sql = `SELECT ${colPg.join(
-      ", "
-    )} FROM users INNER JOIN members ON members.userid = users.userid INNER JOIN projects proj ON proj.projectid = members.projectid`;
+    let sql = `SELECT ${colPg.join(", ")} ${this.sqlPart} ${this.conditional}`;
+    const offsetStr = offset == 0 ? "" : `OFFSET ${offset}`;
+    sql += ` GROUP BY proj.projectid ORDER BY proj.projectid LIMIT ${this.limit} ${offsetStr}`;
 
-    let conditionals = [];
-    let userQuery, subsubquery, subquery;
-    if (constraints.userid) {
-      subsubquery = `SELECT projectid FROM members WHERE userid = ${constraints.userid}`;
-      subquery = `SELECT userid FROM members WHER projectid IN ( ${subsubquery} )`;
-      conditionals.push(
-        `users.userid IN ( ${subquery} ) AND proj.projectid IN ( ${subsubquery} )`
-      );
-    }
+    return this.pool.query(sql);
+  }
 
-    if (constraints.memberName) {
-      userQuery = `SELECT userid FROM users WHERE LOWER(concat(firstname, ' ', lastname)) = LOWER(${constraints.memberName.convert()})`;
-      subsubquery = `SELECT projectid FROM members WHERE userid IN ( ${userQuery} )`;
-      subquery = `SELECT userid FROM members WHERE projectid IN ( ${subsubquery} )`;
-      conditionals.push(
-        `users.userid IN ( ${subquery} ) AND proj.projectid IN ( ${subsubquery} )`
-      );
-    }
+  getAllmember() {
+    let sql = `SELECT concat(users.firstname, ' ', users.lastname) member
+    FROM users 
+    INNER JOIN members ON members.userid = users.userid
+    GROUP BY users.userid ORDER BY users.userid`;
 
-    if (constraints.projectId) {
-      conditionals.push(`proj.projectid = ${constraints.projectId}`);
-    }
-
-    if (constraints.projectName) {
-      conditionals.push(
-        `LOWER(proj.projectname) LIKE %${constraints.projectName
-          .toLowerCase()
-          .convert()}%`
-      );
-    }
+    return this.pool.query(sql);
   }
 };
--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const checkAuth = require('../controller/functions').checkAuth;
+const checkAuth = require("../controller/functions").checkAuth;
+const Project = require("../models/project");
 
-module.exports = (pool,limit=2) => {
+module.exports = (pool, limit = 2) => {
   // get home page of project
-  router.get('/:userid', checkAuth, (req,res) => {
-    res.render('projects', {
-      title: `${req.session.user.firstname} ${req.session.user.lastname} - Projects`,
-      path: '/projects',
-      userid: req.session.user.userid,
-      admin: req.session.user.admin
-    });
+  router.get("/:userid", checkAuth, (req, res) => {
+    const userid = req.session.user.userid;
+    const admin = req.session.user.admin;
+    const current = Number(req.query.page || 1);
+    let url = req.originalUrl;
+    console.log(`original url: ${url}`);
+    url = url.includes("page") ? url : `${url}?page=${current}`;
+    console.log(`modified url: ${url}`);
+    const project = new Project(pool, userid, admin, limit);
+    const pageNumbers = project.getConditional().countPages();
+    const findProjects = project.find(["id", "name", "member"],(current-1)*limit);
+    Promise.all([pageNumbers, findProjects])
+      .then(results => {
+        res.render("projects", {
+          title: `${req.session.user.firstname} ${req.session.user.lastname} - Projects`,
+          path: "/projects",
+          userid,
+          admin,
+          current,
+          url,
+          numOfPages: results[0],
+          data: results[1].rows,
+          colNames: ["#", "Name", "Members"],
+          colPg: ["projectid", "projectname", "member"]
+        });
+      })
+      .catch(e => res.render("error", { message: "Error", error: e }));
   });
-  return router
-}
+  return router;
+};
