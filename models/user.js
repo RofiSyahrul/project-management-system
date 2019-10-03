046c0ecf5da7d5fb7a1f1cd29bc8bbf62bc43788
Rofi Syahrul <srofi44@gmail.com>
2019-10-04 00:37:12 +0700

    modifying user's model: add some methods to find all users with certain conditions from form filters

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 module.exports = class User {
-  constructor(pool, email) {
+  constructor(pool, email, limit = Infinity) {
     this.pool = pool;
     this.email = email;
+    this.limit = limit;
+    this.conditional = "";
+    this.arrConditionals = [];
   }
 
   find() {
@@ -39,8 +42,62 @@ module.exports = class User {
   static getUsers(pool, userid) {
     let conditional = "";
     if (userid) conditional = `WHERE userid = ${userid}`;
-    return pool.query(
-      `SELECT userid, CONCAT(firstname, ' ', lastname) fullname FROM users ${conditional} ORDER BY firstname`
-    );
+    let sql = `SELECT userid, CONCAT(firstname, ' ', lastname) fullname`;
+    sql += ` FROM users ${conditional} ORDER BY firstname`;
+    return pool.query(sql);
+  }
+
+  getConditionals(constraints = {}) {
+    let colNames = Object.keys(constraints);
+    colNames = colNames.filter(col => constraints[col] != undefined);
+
+    let conditionals = [];
+    colNames.forEach((col, i) => {
+      if (col == "fullname") {
+        conditionals.push(`CONCAT(firstname, ' ', lastname) ILIKE $${i + 1}`);
+        this.arrConditionals.push(`%${constraints.fullname}%`);
+      } else if (["nickname", "email"].includes(col)) {
+        conditionals.push(`${col} ILIKE $${i + 1}`);
+        this.arrConditionals.push(`%${constraints[col]}%`);
+      } else {
+        conditionals.push(`${col} = $${i + 1}`);
+        this.arrConditionals.push(constraints[col]);
+      }
+    });
+
+    if (conditionals.length > 0)
+      this.conditional = `WHERE ${conditionals.join(" AND ")}`;
+
+    return this;
+  }
+
+  countPage() {
+    return new Promise((resolve, reject) => {
+      let sql = `SELECT COUNT(*) FROM users ${this.conditional}`;
+      let query =
+        this.conditional.length > 0
+          ? this.pool.query(sql, this.arrConditionals)
+          : this.pool.query(sql);
+      query
+        .then(res => {
+          if (this.limit == Infinity) resolve(res.rows ? 1 : 0);
+          else {
+            resolve(Math.ceil(res.rows ? res.rows[0].count / this.limit : 0));
+          }
+        })
+        .catch(e => reject(e));
+    });
+  }
+
+  findAll(offset = 0) {
+    const limitStr = this.limit == Infinity ? "" : `LIMIT ${this.limit}`;
+    let sql = `SELECT userid, CONCAT(firstname, ' ', lastname) fullname, nickname`;
+    sql += `, email, position, fulltime FROM users ${this.conditional}`;
+    sql += ` ORDER BY userid ${limitStr} OFFSET ${offset}`;
+    let query =
+      this.conditional.length > 0
+        ? this.pool.query(sql, this.arrConditionals)
+        : this.pool.query(sql);
+    return query;
   }
 };
